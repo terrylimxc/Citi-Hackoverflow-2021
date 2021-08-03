6a8c01fa70dda577e53aceda6d2d0614fc3764ff
Low Hon Zheng <76779525+lowhonzheng@users.noreply.github.com>
2021-08-03 16:41:34 +0800

    Update voucher.py

--- classes/voucher.py
+++ classes/voucher.py
@@ -1,9 +1,10 @@
 import uuid
 import qrcode
 class Voucher():
-    def __init__(self, username, amount):
+    def __init__(self, username, amount, vendor_username):
         self.username = username                                     # tagged to customer's username
         self.amount = amount
+        self.vendor_username = vendor_username
         self.voucherID = uuid.uuid1().hex
     def get_voucherID(self):
         return self.voucherID
@@ -11,11 +12,13 @@ class Voucher():
         return self.amount
     def get_username(self):
         return self.username
+    def get_vendor_username(self):
+        return self.vendor_username
     def generate_qr(self):
         qr_generator = qrcode.QRCode(version=1,
                                      box_size=10,
                                      border=5)
-        input_data = f"{self.username},{self.voucherID},{self.amount}"
+        input_data = f"{self.username},{self.voucherID},{self.amount},{self.vendor_username}"
         qr_generator.clear()
         qr_generator.add_data(input_data)
         return qr_generator.make_image() 
