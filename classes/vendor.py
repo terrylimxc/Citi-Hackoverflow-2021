4d1735eade90680f30128d417407cae402e2c266
Low Hon Zheng <76779525+lowhonzheng@users.noreply.github.com>
2021-08-03 15:24:49 +0800

    Update vendor.py
    Added in a new parameter called voucherspurchased to allow vendors to check if the vouchersIDs that the customers are redeeming have already been sold previously

--- classes/vendor.py
+++ classes/vendor.py
@@ -1,14 +1,25 @@
+import cv2 as cv
 class Vendor():
-    def __init__(self,username,password):
+    def __init__(self,username,password,voucherspurchased):
         self.username = username
         self.password = password
-    def scanQR(self, value):                   # input should be "jacky,cf7788eff41511ebb4939cb6d0216836,50"
-        customerUsername, voucherID, amount  = value.split(',')    
+        self.voucherspurchased = voucherspurchased
+    
+    def scanQR(self, qr_image):
+        im = cv.imread(qr_image)
+        det = cv.QRCodeDetector()
+        retval, points, straight_qrcode = det.detectAndDecode(im)
+        customerUsername, voucherID, amount  = retval.split(',')    
         self.useVoucher(customerUsername,voucherID)
             
     # this is called by the vendor when customer uses a voucher - requires username to search for customer in customer database
     # expires the voucher with matching voucherID
     def useVoucher(self,customerUsername,voucherID):
-        return
-        
+        if voucherID in self.voucherspurchased:
+            self.voucherspurchased.remove(voucherID)
+            db.execute('''DELETE FROM users WHERE userID=? AND voucher=?''', (customerUsername, voucherID))
+            db.commit()
+            return "Voucher successfully redeemed!"
+        else:
+            return "Not a valid voucher"        
         # should update customer database/ remove the voucher
