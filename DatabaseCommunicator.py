6be5cf46a3389b0c1366db13399f6ed1ca8588a2
Low Hon Zheng <76779525+lowhonzheng@users.noreply.github.com>
2021-08-03 17:32:51 +0800

    Update DatabaseCommunicator.py

--- DatabaseCommunicator.py
+++ DatabaseCommunicator.py
@@ -4,12 +4,20 @@ from classes.voucher import Voucher
 
 def retrieveCustomer(username, password):
 	# Search customer database and return customer class
-	return Customer()
+	userDataQuery = '''SELECT * FROM users WHERE userID=? AND userPW=?'''
+        loginDataQuery = '''SELECT loyaltyPoint FROM loyalty WHERE userID=? AND userPW=?'''
+        records = db.execute(userDataQuery, (username, password))
+        loyaltyPoint = db.execute(loginDataQuery, (username, password))
+        voucherIDs = [record[2] for record in records]
+        return Customer(username, password, voucherIDs, loyaltyPoint)
 	
 
 def retrieveVendor(username, password):
 	# Search vendor database and return vendor class
-	return Vendor()
+	dataQuery = '''SELECT * FROM vendor WHERE userID=? AND userPW=?'''
+        records = db.execute(dataQuery, (username, password))
+        voucherspurchased = [record[2] for record in records]
+        return Vendor(username, password, voucherspurchased)
         
 # takes the Customer and updates customer database
 def updateCustomer(Customer):        
