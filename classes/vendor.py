249be1295ce05443c67ea4a49007aa0af20f9ca3
Low Hon Zheng <76779525+lowhonzheng@users.noreply.github.com>
2021-08-03 16:01:09 +0800

    Update vendor.py

--- classes/vendor.py
+++ classes/vendor.py
@@ -9,15 +9,17 @@ class Vendor():
         im = cv.imread(qr_image)
         det = cv.QRCodeDetector()
         retval, points, straight_qrcode = det.detectAndDecode(im)
-        customerUsername, voucherID, amount  = retval.split(',')    
-        self.useVoucher(customerUsername,voucherID)
+        customerUsername, voucherID, amount, vendorUsername  = retval.split(',')    
+        self.useVoucher(customerUsername,voucherID,vendorUsername)
             
     # this is called by the vendor when customer uses a voucher - requires username to search for customer in customer database
     # expires the voucher with matching voucherID
-    def useVoucher(self,customerUsername,voucherID):
+    def useVoucher(self,customerUsername,voucherID,vendorUsername):
         if voucherID in self.voucherspurchased:
             self.voucherspurchased.remove(voucherID)
             db.execute('''DELETE FROM users WHERE userID=? AND voucher=?''', (customerUsername, voucherID))
+            db.execute('''DELETE FROM vendor WHERE userID=? AND voucher=?''', (vendorUsername, voucherID))
+            db.execute('''UPDATE overview SET voucherstatus = "Redeemed" WHERE vouchersID=?''', (voucherID,))
             db.commit()
             return "Voucher successfully redeemed!"
         else:
