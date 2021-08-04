cdc85790576dffccf5f7ec606118afdea844e1a0
Low Hon Zheng <76779525+lowhonzheng@users.noreply.github.com>
2021-08-04 18:39:54 +0800

    Update and rename overview.py to vendor.py

---
+++ classes/vendor.py
@@ -0,0 +1,48 @@
+class Vendor():
+  def __init__(username, password):
+    self.username = username
+    self.password = password
+    # self.role = "Overview" # this is for when you want to retrieve the entire voucher database?
+  
+  def show_vouchers(self, username, *filters):
+    # displays all the vouchers in the vouchers table across all vendors
+    # assume filters are a list of vendorUsernames for the overview to only search for some vendors
+    # this is a filter that only shows vendors that are in the filter given
+    # can do more complicated stuff like filtering by expiry date or voucher status if we have time and its feasible
+    # can remove also if its too difficult
+    # returns a list of tuples containing vendorID, voucherID and voucherStatus
+    sqlQuery = '''SELECT vendorID, voucherID, voucherStatus FROM vendors WHERE vendorID = ?'''
+    records = db.execute(sqlQuery, (self.username,))
+    if filters:
+      records = [record for record in records if record[1] in filters]
+      # for this one do we just return the records to the front end or do the backend need to print it out?
+      return records
+    else:
+      return records
+  
+  #for offering a particular denomination of a voucher on the store
+  def postVoucher(self, denomination):
+      # change x and y to something more interpretable?
+      sqlQuery = '''SELECT * FROM vouchers WHERE vendorID = ? AND denomination =?'''
+      # need to check if self.username is correct 
+      x = db.execute(sqlQuery, (self.username, denomination))
+      y = x.rowcount() > 0
+      #need to check if the denomination + vendor name is already in the database
+      if not y:
+          db.execute("insert into vouchers values (vendorID, denomination) values (?, ?)",
+                    (self.username, denomination))
+          return "Voucher successfully added!"
+      else:
+          return "Voucher is already on sale!"
+  
+  # for removing a particular denomination of a voucher from the store    
+  def removeVoucher(self, denomination):
+      sqlQuery = '''SELECT * FROM vouchers WHERE vendorID = ? AND denomination = ?'''
+      x = db.execute(sqlQuery, (self.username, denomination)
+      y = x.rowcount()>0
+      #need to check if the denomination + vendor name is in the database
+      if y:
+          db.execute("delete from vouchers where vendorID = ? and denomination = ?", (self.username, denomination))
+          return "Voucher successfully removed!"
+      else:
+          return "Voucher is not currently on sale!"
