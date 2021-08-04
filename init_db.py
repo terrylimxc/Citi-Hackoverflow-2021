ef725ecc10968c68fa40bedb3a327fae20544795
paulchew <76769009+paulchew@users.noreply.github.com>
2021-08-04 15:47:24 +0800

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -5,8 +5,6 @@
 #vendors for allowing vendors to check vocuhers they have issued along with the voucher's information (col names: vendorID, vendorPW, voucherID, voucherStatus, voucherAmount)
 #vouchers for keeping track of the types of vouchers that are currently on sale (col names: vendorname, denomination)
 
-# need to confirm if vendorname = vendorID and standardise, and ask if we shd change denomination type to int
-
 import sqlite3
 
 conn = sqlite.connect('database.db')
@@ -15,7 +13,7 @@ db = conn.cursor()
 db.execute('''create table loyalty (userID text, loyaltyPoints int)''')
 db.execute('''create table users (userID text, userPW text, voucherID text, voucherAmount int, vendorID text)''')
 db.execute('''CREATE TABLE vendors (vendorID text, vendorPW text, voucherID text, voucherStatus text, voucherAmount int)''') #the 2 status names will be Purchased and Redeemed
-db.execute('''CREATE table vouchers (vendorname text, denomination text)''') # shd we change denomination type to int?
+db.execute('''CREATE table vouchers (vendorID text, denomination int)''')
 
 db.commit()
 db.close()
