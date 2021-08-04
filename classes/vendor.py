3614d52f26d5f15ca0fba14ec9965f719ecc266f
Low Hon Zheng <76779525+lowhonzheng@users.noreply.github.com>
2021-08-04 19:37:03 +0800

    Update vendor.py

--- classes/vendor.py
+++ classes/vendor.py
@@ -4,21 +4,34 @@ class Vendor():
     self.password = password
     # self.role = "Overview" # this is for when you want to retrieve the entire voucher database?
   
-  def show_vouchers(self, username, *filters):
+  def show_vouchers(self, *filters):
     # displays all the vouchers in the vouchers table across all vendors
     # assume filters are a list of vendorUsernames for the overview to only search for some vendors
     # this is a filter that only shows vendors that are in the filter given
     # can do more complicated stuff like filtering by expiry date or voucher status if we have time and its feasible
     # can remove also if its too difficult
     # returns a list of tuples containing vendorID, voucherID and voucherStatus
-    sqlQuery = '''SELECT vendorID, voucherID, voucherStatus FROM vendors WHERE vendorID = ?'''
+    #merged = db.execute('''SELECT vendors.vendorID, vendors.voucherID, vendors.voucherStatus, records1.userID, records1.voucherAmount FROM records LEFT JOIN records1 ON records.voucherID = records1.voucherID'''
+    sqlQuery = '''SELECT vendorID, voucherID, voucherStatus, userID, voucherAmount FROM vendors WHERE vendorID = ?'''
     records = db.execute(sqlQuery, (self.username,))
+    #secondQuery = '''SELECT userID, voucherID, voucherAmount FROM users WHERE vendorID = ?'''
+    #records1 = db.execute(secondQuery, (self.username,))
+    #finalrecords = db.execute('''SELECT records.vendorID, records.voucherID, records.voucherStatus, records1.userID, records1.voucherAmount FROM records LEFT JOIN records1 ON records.voucherID = records1.voucherID'''
+    ans = []
     if filters:
-      records = [record for record in records if record[1] in filters]
+      records = [record for record in records if record[2] in filters]
       # for this one do we just return the records to the front end or do the backend need to print it out?
-      return records
+      for record in records:
+        temp = Voucher(record[3], record[4], record[0])
+        temp.voucherID = record[1]
+        ans.append(temp)
+      return ans
     else:
-      return records
+      for record in records:
+        temp = Voucher(record[3], record[4], record[0])
+        temp.voucherID = record[1]
+        ans.append(temp)
+      return ans
   
   #for offering a particular denomination of a voucher on the store
   def postVoucher(self, denomination):
@@ -31,6 +44,7 @@ class Vendor():
       if not y:
           db.execute("insert into vouchers values (vendorID, denomination) values (?, ?)",
                     (self.username, denomination))
+          db.commit()
           return "Voucher successfully added!"
       else:
           return "Voucher is already on sale!"
@@ -43,6 +57,7 @@ class Vendor():
       #need to check if the denomination + vendor name is in the database
       if y:
           db.execute("delete from vouchers where vendorID = ? and denomination = ?", (self.username, denomination))
+          db.commit()
           return "Voucher successfully removed!"
       else:
           return "Voucher is not currently on sale!"
